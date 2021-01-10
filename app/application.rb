b5f0ac25985bfed0c9575661fabb9ef4f00191c5
github username <github email address>
2021-01-09 22:38:22 -0500

    commit tree fd32a4245177df02433ea9e91420db2718a5a1fc

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,16 @@
 class Application
 
-def call(env)
-    resp = Rack::Response.new
-    time = Time.now.hour.to_i
+    def call(env)
+        resp = Rack::Response.new
+        time = Time.now.hour.to_i
 
-    if time < 12
-        resp.write "Good Morning!"
-    else
-        resp.write "Good Afternoon!"
+        # resp.write Time.now.hour
+        if time < 12
+            resp.write "Good Morning!"
+        else
+            resp.write "Good Afternoon!"
+        end
+        resp.finish
     end
-    resp.finish
-end
 
 end
